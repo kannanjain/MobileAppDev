1849b05b31275e1fa0044901a425a617cfb06877
kannanjain <kannanjain4@gmail.com>
2025-03-05 14:43:58 -0800

    configs

--- JournalApp/app/src/main/java/com/zybooks/journalapp/MainActivity.kt
+++ JournalApp/app/src/main/java/com/zybooks/journalapp/MainActivity.kt
@@ -52,8 +52,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.viewmodel.compose.viewModel
 
-class cd ..
-MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
