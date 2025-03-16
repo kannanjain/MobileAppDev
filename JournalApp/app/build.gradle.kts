04f4f2b85d33e8940e854741330addf64fc40db4
Sriya Adunur <sadunur@calpoly.edu>
2025-03-15 20:45:19 -0700

    Added new dependencies for location and camera

--- JournalApp/app/build.gradle.kts
+++ JournalApp/app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation(libs.play.services.location)
     implementation(libs.play.services.tasks)
     implementation(libs.androidx.runtime.livedata)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -64,5 +66,11 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation(libs.play.services.tasks)
     implementation(libs.androidx.runtime.livedata)
+    implementation("io.coil-kt:coil-compose:2.6.0") // Use the latest version
+    implementation("androidx.camera:camera-camera2:1.2.3") // or a newer version
+    implementation("androidx.camera:camera-lifecycle:1.2.3")
+    implementation("androidx.camera:camera-view:1.2.3")
+    implementation("androidx.compose.material:material-icons-extended:1.3.0") // or the appropriate version you're using
+    implementation("androidx.compose.material:material:1.3.0") // For core material components
 
 }
